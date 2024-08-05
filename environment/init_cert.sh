b427e745131a3060c5190e69f41092465fcfc4ae
Despane <a.v.kochetov@icloud.com>
2024-08-05 12:15:13 +0300

    test server

--- environment/init_cert.sh
+++ environment/init_cert.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(bitcoder.ru bitcoder.ru)
+domains=(bitcoder.ru www.bitcoder.ru)
 rsa_key_size=4096
 data_path="./certbot"
 email="letsencryptcerts@bitcoder.ru" # Adding a valid address is strongly recommended
@@ -16,15 +16,6 @@ if [ -d "$data_path" ]; then
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
-fi
-
-
-if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-  echo "### Downloading recommended TLS parameters ..."
-  mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
-  echo
 fi
 
 echo "### Creating dummy certificate for $domains ..."
